b50810199878724b3f237633202bfebb4fb1d1ba
sergey@telminov.ru <sergey@telminov.ru>
2016-02-18 20:20:55 +0300

    - error process  - command line test support

--- sms_devino/client.py
+++ sms_devino/client.py
@@ -14,22 +14,28 @@ METHOD_POST = 'post'
 
 
 class DevinoException(Exception):
-    pass
+    def __init__(self, message: str, http_status: int, description: str = None, code: int = None,
+                 base_exception: Exception = None):
+        self.message = message
+        self.http_status = http_status
+        self.description = description
+        self.code = code
+        self.base_exception = base_exception
 
 
 class DevinoClient:
 
-    def __init__(self, login, password, url=REST_URL):
+    def __init__(self, login: str, password: str, url: str =REST_URL):
         self.login = login
         self.password = password
         self.url = url
         self._session_id = None
 
-    def get_balance(self):
+    def get_balance(self) -> float:
         session_id = self._get_session_id()
         return self._request(BALANCE_URL, {'sessionId': session_id})
 
-    def _get_session_id(self):
+    def _get_session_id(self) -> str:
         if not self._session_id:
             self._session_id = self._request(SESSION_URL, {'login': self.login, 'password': self.password})
         return self._session_id
@@ -43,8 +49,22 @@ class DevinoClient:
             response = requests.post(request_url, data=params)
 
         if 400 <= response.status_code <= 500:
-            # TODO
-            raise DevinoException()
+            error_description = response.json()
+            raise DevinoException(
+                message='Ошибка отправки',
+                http_status=response.status_code,
+                description=error_description.get('Desc'),
+                code=error_description.get('Code'),
+            )
 
         return response.json()
 
+
+
+if __name__ == '__main__':
+    # quick test: python client.py tester 123
+    import sys
+    login = sys.argv[1]
+    passwd = sys.argv[2]
+    client = DevinoClient(login, passwd)
+    print(client.get_balance())
